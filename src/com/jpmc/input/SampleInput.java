d772dedc461a995cda1187504e5b6d0f31949ad0
debasishma <39435421+debasishma@users.noreply.github.com>
2019-05-08 22:33:01 +0100

    Update SampleInput.java

--- src/com/jpmc/input/SampleInput.java
+++ src/com/jpmc/input/SampleInput.java
@@ -24,19 +24,19 @@ public class SampleInput {
 						new InstructionData(Currency.getInstance("GBP"), BigDecimal.valueOf(1.12), 450,
 								BigDecimal.valueOf(150.5))),
 
-				new Instruction("Amazon", InstructionType.SELL, LocalDate.of(2018, 3, 09), LocalDate.of(2018, 3, 17),
+				new Instruction("Amazon", InstructionType.SELL, LocalDate.of(2018, 3, 9), LocalDate.of(2018, 3, 17),
 						new InstructionData(Currency.getInstance("SAR"), BigDecimal.valueOf(0.27), 150,
 								BigDecimal.valueOf(400.8))),
 
-				new Instruction("Google", InstructionType.SELL, LocalDate.of(2019, 5, 09), LocalDate.of(2019, 5, 20),
+				new Instruction("Google", InstructionType.SELL, LocalDate.of(2019, 5, 9), LocalDate.of(2019, 5, 20),
 						new InstructionData(Currency.getInstance("EUR"), BigDecimal.valueOf(0.34), 50,
 								BigDecimal.valueOf(500.6))),
 
-				new Instruction("UAEBank", InstructionType.BUY, LocalDate.of(2017, 1, 04), LocalDate.of(2017, 1, 06),
+				new Instruction("UAEBank", InstructionType.BUY, LocalDate.of(2017, 1, 4), LocalDate.of(2017, 1, 06),
 						new InstructionData(Currency.getInstance("AED"), BigDecimal.valueOf(0.22), 450,
 								BigDecimal.valueOf(150.5))),
 
-				new Instruction("SingFed", InstructionType.BUY, LocalDate.of(2016, 1, 01), LocalDate.of(2016, 1, 02),
+				new Instruction("SingFed", InstructionType.BUY, LocalDate.of(2016, 1, 1), LocalDate.of(2016, 1, 02),
 						new InstructionData(Currency.getInstance("SGD"), BigDecimal.valueOf(0.50), 200,
 								BigDecimal.valueOf(100.25))),
 
@@ -44,7 +44,7 @@ public class SampleInput {
 						new InstructionData(Currency.getInstance("EUR"), BigDecimal.valueOf(0.34), 1000,
 								BigDecimal.valueOf(210.6))),
 
-				new Instruction("StanC", InstructionType.SELL, LocalDate.of(2018, 10, 06), LocalDate.of(2018, 10, 13),
+				new Instruction("StanC", InstructionType.SELL, LocalDate.of(2018, 10, 6), LocalDate.of(2018, 10, 13),
 						new InstructionData(Currency.getInstance("EUR"), BigDecimal.valueOf(0.34), 120,
 								BigDecimal.valueOf(339.5)))));
 	}
